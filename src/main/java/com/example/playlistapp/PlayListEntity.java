c042cafef740a77b5be5f4b140fbd0231141c342
Samik <3612424+samik-saha@users.noreply.github.com>
2021-04-10 13:36:30 -0400

    Recommit: IT test for get playlist with no songs - Green

--- src/main/java/com/example/playlistapp/PlayListEntity.java
+++ src/main/java/com/example/playlistapp/PlayListEntity.java
@@ -1,14 +1,18 @@
 package com.example.playlistapp;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @EqualsAndHashCode
+@Getter
 public class PlayListEntity {
 
     @Id
@@ -17,9 +21,16 @@ public class PlayListEntity {
 
     String name;
 
+    ArrayList<String> songs= new ArrayList<>();
+
     public PlayListEntity(String name) {
         this.name = name;
     }
 
+    public PlayListEntity() {
+    }
 
+    public ArrayList<String> getSongs() {
+        return songs;
+    }
 }
