4af1cebc7af21c9b9ce85261613bde213d009908
varadharajanthiagarajan <varadha.001@gmail.com>
2021-04-10 11:14:11 -0400

    Green Test case - add playlist controller with empty method

---
+++ src/main/java/com/example/playlistapp/PlayListController.java
@@ -0,0 +1,18 @@
+package com.example.playlistapp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PlayListController {
+
+    @PostMapping("playlist")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addPlayList(){
+
+    }
+}
